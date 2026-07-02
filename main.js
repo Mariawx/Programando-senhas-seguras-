18effb6867712d23f4e29c349284cca14bb9628a
Mariawx <grzeszcyszen.maria@escola.pr.gov.br>
2026-07-02 10:19:12 -0300

    .

--- main.js
+++ main.js
@@ -12,5 +12,35 @@ numeroSenha.textContent = tamanhoSenha;
 function aumentaTamanho(){
     tamanhoSenha = tamanhoSenha+1;
     numeroSenha.textContent = tamanhoSenha;
+
+    function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+
+    function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       tamanhoSenha = tamanhoSenha+1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+
+    function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+
+    function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+}
+}
+}
 }
 }
